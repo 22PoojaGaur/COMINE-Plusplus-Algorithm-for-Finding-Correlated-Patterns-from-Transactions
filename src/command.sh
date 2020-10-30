be892f6b54a37b50fe3d6af0b6f4b79de9b935b1
Pooja Gaur <poojagaur2293@gmail.com>
2020-10-30 20:01:34 +0530

    added header and parent structure. Wrote skeleton of comine_k_algo

--- src/command.sh
+++ src/command.sh
@@ -1,2 +1,2 @@
-python3 algorithm_1.py ../data/grocery_transactions.txt > ../data/algo_1.txt
-#python3 algorithm_1.py ../data/example_transaction.txt > ../data/example_algo_1.txt
+#python3 algorithm_1.py ../data/grocery_transactions.txt > ../data/algo_1.txt
+python3 algorithm_1.py ../data/example_transaction.txt > ../data/example_algo_1.txt
